1dd0272fdbefe5a2d3547c56abce77016e914a5b
Torben Nordtorp <torbenn@axis.com>
2026-07-24 10:17:13 +0200

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-const tseslint = require("typescript-eslint");
+import tseslint from "typescript-eslint";
 
-module.exports = tseslint.config(
+export default tseslint.config(
   {
     ignores: ["node_modules/**", "renders/**"],
   },
